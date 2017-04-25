0908853b3fd909bc220841445cdac402c1e84796
lauris6262 <lauradulce@gmail.com>
2017-04-24 21:54:34 -0500

    Update and rename my_code.sh to my_code_lauraR.sh

---
+++ my_code_lauraR.sh
@@ -0,0 +1,45 @@
+echo "Hello World!"
+salak405@SALAK405-32 MINGW64 ~
+$ git clone https://github.com/amartinezg/git-repo.git
+Cloning into 'git-repo'...
+remote: Counting objects: 9, done.
+remote: Compressing objects: 100% (2/2), done.
+remote: Total 9 (delta 1), reused 9 (delta 1), pack-reused 0
+Unpacking objects: 100% (9/9), done.
+
+salak405@SALAK405-32 MINGW64 ~
+$ ll
+total 15949
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:05 AppData/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:06 Contacts/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:10 Desktop/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:53 Documents/
+drwxr-xr-x 1 salak405 1049089 0 abr. 24 20:39 Downloads/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:06 Favorites/
+drwxr-xr-x 1 salak405 1049089 0 abr. 24 21:48 git-repo/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:06 Links/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:06 Music/
+-rw-r--r-- 1 salak405 1049089 6815744 feb. 23 11:21 NTUSER.DAT
+-rw-r--r-- 1 salak405 1049089 1716224 ene. 23 11:05 ntuser.dat.LOG1
+-rw-r--r-- 1 salak405 1049089 557056 ene. 23 11:05 ntuser.dat.LOG2
+-rw-r--r-- 1 salak405 1049089 65536 ene. 23 12:04 NTUSER.DAT{f5b13604-4b48-11e6-80cb-e41d2d012050}.TM.blf
+-rw-r--r-- 1 salak405 1049089 524288 ene. 23 12:04 NTUSER.DAT{f5b13604-4b48-11e6-80cb-e41d2d012050}.TMContainer00000000000000000001.regtrans-ms
+-rw-r--r-- 1 salak405 1049089 524288 ene. 23 12:04 NTUSER.DAT{f5b13604-4b48-11e6-80cb-e41d2d012050}.TMContainer00000000000000000002.regtrans-ms
+-rw-r--r-- 1 salak405 1049089 20 ene. 23 11:05 ntuser.ini
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:09 OneDrive/
+drwxr-xr-x 1 salak405 1049089 0 feb. 23 09:51 Oracle/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:07 Pictures/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:06 'Saved Games'/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:07 Searches/
+drwxr-xr-x 1 salak405 1049089 0 ene. 23 11:06 Videos/
+
+salak405@SALAK405-32 MINGW64 ~
+$ cd git-repo/
+
+salak405@SALAK405-32 MINGW64 ~/git-repo (master)
+$ git checkout
+Your branch is up-to-date with 'origin/master'.
+
+salak405@SALAK405-32 MINGW64 ~/git-repo (master)
+$ git branch
+* master
